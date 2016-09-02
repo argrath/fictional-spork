a57e36c770791f84c9f158e64575364f4cf15f67
SHIRAKATA Kentaro <argrath@ub32.org>
2016-09-03 04:51:41 +0900

    in progress

--- build.pl
+++ build.pl
@@ -7,8 +7,11 @@ use Text::MultiMarkdown 'markdown';
 use Data::Dumper;
 
 use Template;
+use Time::Piece;
 
 my @file;
+
+my $fnpat = 'output/%Y/%m/%d-%M.html';
 {
     {
 	open my $f, '<', $ARGV[0];
@@ -36,14 +39,25 @@ my @file;
 
     $vars{title} = $yaml->[0]->{title};
     {
+	my (@tags) = split / /, $yaml->[0]->{tags};
+	my @tagstr = ();
+	for(@tags){
+	    my $tag = lc($_);
+	    push @tagstr, sprintf('<a href="../../%s">%s</a>', $tag, $tag);
+	}
+	$vars{tags} = join(', ', @tagstr);
+    }
+    my $outfn;
+    {
 	my $date = $yaml->[0]->{date};
+	my $dateobj = Time::Piece->strptime($date, '%Y-%m-%d %H:%M:%S');
 	$vars{date} = $date;
 	substr($date, 10, 1) = 'T';
 	$date .= '+0900';
 	$vars{w3cdate} = $date;
+	$outfn = $dateobj->strftime($fnpat);
     }
 
     my $tt = new Template;
-    $tt->process('tmpl/entry.tt.html', \%vars);
-
+    $tt->process('tmpl/entry.tt.html', \%vars, $outfn);
 }
