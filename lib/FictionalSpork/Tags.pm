e07eb94770a49070d11f7249a86b69db457320ef
SHIRAKATA Kentaro <argrath@ub32.org>
2018-07-17 22:49:50 +0900

    add Tags

---
+++ lib/FictionalSpork/Tags.pm
@@ -0,0 +1,36 @@
+package FictionalSpork::Tags;
+
+use warnings;
+use strict;
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+
+    my $tags = shift;
+    {
+	my (@tags) = split / /, $meta->{tags};
+	my @tagobj = ();
+	for(@tags){
+	    my $tag = FictionalSpork::Tag->new($_);
+	    push @tagobj, $tag;
+	}
+	$self->{tags} = \@tagobj;
+    }
+
+    return $self;
+}
+
+sub to_string {
+    my $self = shift;
+    my $tag = $self->{tags};
+
+    my @tagstr = ();
+    for(@{$self->{tags}}){
+	push @tagstr, $_->url;
+    }
+    return join(', ', @tagstr);
+}
+
+1;
