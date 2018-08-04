ecaff5e66859ec8e150f14536471008b97fb3bbe
SHIRAKATA Kentaro <argrath@ub32.org>
2018-08-04 16:23:31 +0900

    change indent

--- lib/FictionalSpork/Atom.pm
+++ lib/FictionalSpork/Atom.pm
@@ -10,25 +10,25 @@ use XML::FeedPP;
 my $siteurl = 'https://argrath.ub32.org/annex/';
 
 sub atom {
-	my (@list) = @_;
+    my (@list) = @_;
 
-	my $conf = FictionalSpork::Conf::get();
+    my $conf = FictionalSpork::Conf::get();
 
     my $f = XML::FeedPP::Atom::Atom10->new(
-	title => $conf->{title},
-	description => $conf->{description},
-	pubDate => time(),
-	link => $siteurl,
-	copyright => $conf->{copyright},
-	lang => 'ja',
-	);
+        title => $conf->{title},
+        description => $conf->{description},
+        pubDate => time(),
+        link => $siteurl,
+        copyright => $conf->{copyright},
+        lang => 'ja',
+        );
 
     for(@list){
-	$f->add_item(
-	    title => $_->{title},
-	    pubDate => $_->{w3cdate},
-	    link => $siteurl . $_->{link},
-	    );
+        $f->add_item(
+            title => $_->{title},
+            pubDate => $_->{w3cdate},
+            link => $siteurl . $_->{link},
+            );
     }
 
     $f->to_file('output/atom.xml');
