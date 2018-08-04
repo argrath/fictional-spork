e4e9060629caab47e7446d242f9f160f9125a593
SHIRAKATA Kentaro <argrath@ub32.org>
2018-08-05 02:35:45 +0900

    separate build process

---
+++ lib/FictionalSpork/Command/Build.pm
@@ -0,0 +1,69 @@
+package FictionalSpork::Command::Build;
+
+use strict;
+use warnings;
+
+use lib 'lib';
+
+use YAML::Tiny;
+
+use Data::Dumper;
+
+use Template;
+use Time::Piece;
+
+use FictionalSpork::Mods;
+use FictionalSpork::Entry;
+use FictionalSpork::Tag;
+use FictionalSpork::Markdown;
+use FictionalSpork::Conf;
+
+sub build {
+    my $fn = shift;
+
+    my $conf = FictionalSpork::Conf::get();
+
+    my $entry = FictionalSpork::Entry->new($fn);
+    my (@file) = @{$entry->file};
+
+    my $meta = $entry->meta;
+
+    my %vars;
+
+    $file[2] =~ s/\n([ \t]*)```([a-z]*)\n/\n$1```\n/g;
+    my $html1 = FictionalSpork::Markdown::make_markdown($file[1]);
+
+    $vars{text} = $html1;
+
+#    $file[2] =~ s{\n```([a-z]*)\n(.+?)\n```\n}{<pre><code>$2</code></pre>}sg;
+#    $file[2] =~ s/\n([ \t]*)```([a-z]*)\n/\n$1```\n/g;
+    my $html2 = FictionalSpork::Markdown::make_markdown($file[2]);
+
+    $vars{textmore} = $html2;
+
+    $vars{blogtitle} = $conf->{title};
+    $vars{title} = $meta->{title};
+    $vars{w3cdate} = $meta->{w3cdate};
+    {
+	my (@tags) = split / /, $meta->{tags};
+	my @tagstr = ();
+	for(@tags){
+	    my $tag = FictionalSpork::Tag->new($_);
+	    push @tagstr, $tag->url;
+	}
+	$vars{tags} = join(', ', @tagstr);
+    }
+    my $outfn;
+    {
+	my $date = $meta->{date};
+	$vars{date} = $date;
+
+	my $fnpat = 'output/%Y/%m/%d-%H.html';
+	$outfn = FictionalSpork::Mods::entry_url($date, $fnpat);
+    }
+
+    my $tt = new Template;
+    $tt->process('tmpl/entry.tt.html', \%vars, $outfn, 'binmode' => ':utf8');
+}
+
+1;
